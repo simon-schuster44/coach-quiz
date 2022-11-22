9ea5c745d558fb0e4d2dfef3599ba8b42cde0a55
Simon Schuster <simschu@gmx.de>
2022-11-22 10:45:26 +0100

    added stupid containers

--- src/App.js
+++ src/App.js
@@ -15,6 +15,53 @@ function App() {
         START
       </Button>
 
+      <TextContainer
+        left="15%"
+        top="200px"
+        rotate="-25deg"
+        questionId={questionId}
+      >
+        Wow!
+      </TextContainer>
+      <TextContainer
+        top="60%"
+        left="20%"
+        rotate="-8deg"
+        questionId={questionId}
+      >
+        It's real!
+      </TextContainer>
+      <TextContainer
+        top="60%"
+        left="70%"
+        rotate="-15deg"
+        questionId={questionId}
+      >
+        Super!
+      </TextContainer>
+      <TextContainer
+        top="150px"
+        left="75%"
+        rotate="10deg"
+        questionId={questionId}
+      >
+        Amazing!
+      </TextContainer>
+      <TextContainer top="80%" left="40%" rotate="5deg" questionId={questionId}>
+        NEW!
+      </TextContainer>
+      <TextContainer top="80%" left="85%" rotate="5deg" questionId={questionId}>
+        OMG!
+      </TextContainer>
+      <TextContainer
+        top="87%"
+        left="10%"
+        rotate="-3deg"
+        questionId={questionId}
+      >
+        Unbelievable!
+      </TextContainer>
+
       <Question id={questionId} setQuestionId={setQuestionId} />
       <Footer>
         <p>Created with ☕️ by B4CKSL4SH</p>
@@ -52,6 +99,24 @@ const Button = styled.button`
   box-shadow: 2px 2px 8px black;
   z-index: 1;
   ${(props) => (props.questionId === 0 ? "" : "z-index: -1; opacity: 0")}
+`;
+
+const TextContainer = styled.div`
+  background-color: #f45b69;
+  padding: 5px 10px;
+  height: 50px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 30px;
+  border-radius: 20px;
+  box-shadow: 0 0 12px black;
+  position: absolute;
+  transition: 0.5s;
+  ${(props) => (props.questionId === 0 ? "" : "z-index: -1; opacity: 0")};
+  ${(props) => (props.left ? `left: ${props.left}` : "")};
+  ${(props) => (props.top ? `top: ${props.top}` : "")};
+  ${(props) => (props.rotate ? `transform: rotate(${props.rotate})` : "")};
 `;
 
 const Footer = styled.footer`
